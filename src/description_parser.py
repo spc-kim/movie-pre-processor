aa360a971ee04d1622558024d4e7daab7cd3479b
Kyung Jae Kim <kyung_jae.kim@spaceforce.mil>
2026-09-02 12:24:01 -0400

    Working script for entire example description

--- src/description_parser.py
+++ src/description_parser.py
@@ -1,3 +1,17 @@
+# Get our text parsing functions from other file and set keyword
 import text_parsing_functions as tpf
 
-stopwords = tpf.load_stopwords('data/stopwords.txt')
+if __name__ == '__main__':
+
+    # Set test arguments
+    stopwords = tpf.load_stopwords('data/stopwords.txt')
+    name_set = set(['seokwoo', 'suan', 'seongkyeong', 'busan'])
+    replace_val = 'person'
+
+    # Open the example description file
+    with open('data/train_to_busan_description.txt', 'r') as file_obj:
+        sample_text = file_obj.read()
+
+    # Run our pipeline and print it
+    cleaned_text = tpf.line_cleaning_pipeline(sample_text, stopwords, name_set, replace_val)
+    print(cleaned_text)
